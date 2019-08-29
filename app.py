8609612044b38bec21a24cbaa9d58c3d4365e9ef
jijin-github <jijin.sbstn@gmail.com>
2019-08-29 08:25:06 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,124 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy.dialects.mysql import TIME
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    code = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<Country %r>' % self.name
+
+
+class State(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'),
+                            nullable=False)
+    name = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<State %r>' % self.name
+
+
+class Place(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    state_id = db.Column(db.Integer, db.ForeignKey('state.id'),
+                           nullable=False)
+    name = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<Place %r>' % self.name
+
+
+class RestaurantCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<RestaurantCategory %r>' % self.name
+
+
+class TableCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Category %r>' % self.title
+
+
+class Table(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('TableCategory.id'),
+                            nullable=False)
+    category = db.relationship('tablecategory',
+                               backref=db.backref('restaurant', lazy=True))
+    seat_count = db.Column(db.Integer, nullable=True)
+    count = db.Column(db.Integer, nullable=True)
+
+    def __repr__(self):
+        return '<Table %r>' % self.title
+
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('restaurantcategory.id'),
+                         nullable=False)
+    category = db.relationship('RestaurantCategory',
+                               backref=db.backref('restaurant', lazy=True))
+
+    place_id = db.Column(db.Integer, db.ForeignKey('place.id'),
+                           nullable=False)
+    place = db.relationship('Place',
+                               backref=db.backref('restaurant', lazy=True))
+    title = db.Column(db.String(128))
+    description = db.Column(db.Text, nullable=True)
+    mobile = db.Column(db.Integer, nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    opening_time = db.Column(TIME(), nullable=False)
+    closing_time = db.Column(TIME(), nullable=False)
+    active = db.Column(db.Boolean(), default=True)
+
+    def __repr__(self):
+        return '<Restaurant %r>' % self.title
+
+
+tables = db.Table('tags',
+    db.Column('table_id', db.Integer, db.ForeignKey('table.id'), primary_key=True),
+    db.Column('booked_id', db.Integer, db.ForeignKey('booktable.id'), primary_key=True)
+)
+
+
+class BookTable(db.Model):
+    user_email = db.Column(db.String(120), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'),
+                            nullable=False)
+    restaurant = db.relationship('Restaurant',
+                               backref=db.backref('restaurant', lazy=True))
+
+    tables = db.relationship('Table', secondary=tables, lazy='subquery',
+                           backref=db.backref('booked_tables', lazy=True))
+    booked_time = db.Column(db.DateTime, nullable=False,
+        default=datetime.utcnow)
+    spend_hour = db.Column(db.Integer, default=2)
+
+
+
+
+# db.create_all()
+# db.session.add(User(username="Flask", email="example@example.com"))
+# db.session.add(User(username="Flask2", email="example2@example.com"))
+# db.session.commit()
+#
+# users = User.query.all()
+# print(users)
